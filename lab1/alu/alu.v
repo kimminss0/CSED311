65e577fce4db0300b829a835eee9d76d392eb866
Minseo Kim <kimminss0@outlook.kr>
2025-02-25 20:18:44 +0900

    feat(lab1/alu): implement working alu

--- lab1/alu/alu.v
+++ lab1/alu/alu.v
@@ -6,21 +6,39 @@ module ALU #(parameter data_width = 16) (
        	output reg OverflowFlag);
 // Do not use delay in your implementation.
 
-// You can declare any variables as needed.
-/*
-	YOUR VARIABLE DECLARATION...
-*/
-
 initial begin
 	C = 0;
 	OverflowFlag = 0;
 end   	
 
-// TODO: You should implement the functionality of ALU!
-// (HINT: Use 'always @(...) begin ... end')
-/*
-	YOUR ALU FUNCTIONALITY IMPLEMENTATION...
-*/
+always @(*) begin
+	if (FuncCode == 4'b0000)
+		OverflowFlag = A[data_width - 1] == B[data_width - 1] && A[data_width - 1] != C[data_width - 1];
+	else if (FuncCode == 4'b0001)
+		OverflowFlag = A[data_width - 1] != B[data_width - 1] && A[data_width - 1] != C[data_width - 1];
+	else
+		OverflowFlag = 0;
+end
+
+always @(*) begin
+	case (FuncCode)
+		4'b0000 : C = A + B;
+		4'b0001 : C = A - B;
+		4'b0010 : C = A;
+		4'b0011 : C = ~A;
+		4'b0100 : C = A & B;
+		4'b0101 : C = A | B;
+		4'b0110 : C = ~(A & B);
+		4'b0111 : C = ~(A | B);
+		4'b1000 : C = A ^ B;
+		4'b1001 : C = ~(A ^ B);
+		4'b1010 : C = A << 1;
+		4'b1011 : C = A >> 1;
+		4'b1100 : C = A <<< 1;
+		4'b1101 : C = A >>> 1;
+		4'b1110 : C = ~A + 1;
+		default : C = 0;
+	endcase
+end
 
 endmodule
-
