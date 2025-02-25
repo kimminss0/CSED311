6fe84d9c1cbdd4969356c2379d2f9d14de2a6f8c
Minseo Kim <kimminss0@outlook.kr>
2025-02-25 22:26:05 +0900

    style(lab1/alu): use macros defined in alu_func.v

--- lab1/alu/alu.v
+++ lab1/alu/alu.v
@@ -1,3 +1,5 @@
+`include "alu_func.v"
+
 module ALU #(parameter data_width = 16) (
 	input [data_width - 1 : 0] A, 
 	input [data_width - 1 : 0] B, 
@@ -12,32 +14,32 @@ initial begin
 end   	
 
 always @(*) begin
-	if (FuncCode == 4'b0000)
-		OverflowFlag = A[data_width - 1] == B[data_width - 1] && A[data_width - 1] != C[data_width - 1];
-	else if (FuncCode == 4'b0001)
-		OverflowFlag = A[data_width - 1] != B[data_width - 1] && A[data_width - 1] != C[data_width - 1];
-	else
-		OverflowFlag = 0;
+	case (FuncCode)
+		`FUNC_ADD : OverflowFlag = A[data_width - 1] == B[data_width - 1] && A[data_width - 1] != C[data_width - 1];
+		`FUNC_SUB : OverflowFlag = A[data_width - 1] != B[data_width - 1] && A[data_width - 1] != C[data_width - 1];
+		default   : OverflowFlag = 0;
+	endcase
 end
 
 always @(*) begin
 	case (FuncCode)
-		4'b0000 : C = A + B;
-		4'b0001 : C = A - B;
-		4'b0010 : C = A;
-		4'b0011 : C = ~A;
-		4'b0100 : C = A & B;
-		4'b0101 : C = A | B;
-		4'b0110 : C = ~(A & B);
-		4'b0111 : C = ~(A | B);
-		4'b1000 : C = A ^ B;
-		4'b1001 : C = ~(A ^ B);
-		4'b1010 : C = A << 1;
-		4'b1011 : C = A >> 1;
-		4'b1100 : C = A <<< 1;
-		4'b1101 : C = A >>> 1;
-		4'b1110 : C = ~A + 1;
-		default : C = 0;
+		`FUNC_ADD  : C = A + B;
+		`FUNC_SUB  : C = A - B;
+		`FUNC_ID   : C = A;
+		`FUNC_NOT  : C = ~A;
+		`FUNC_AND  : C = A & B;
+		`FUNC_OR   : C = A | B;
+		`FUNC_NAND : C = ~(A & B);
+		`FUNC_NOR  : C = ~(A | B);
+		`FUNC_XOR  : C = A ^ B;
+		`FUNC_XNOR : C = ~(A ^ B);
+		`FUNC_LLS  : C = A << 1;
+		`FUNC_LRS  : C = A >> 1;
+		`FUNC_ALS  : C = A <<< 1;
+		`FUNC_ARS  : C = A >>> 1;
+		`FUNC_TCP  : C = ~A + 1;
+		`FUNC_ZERO : C = 0;
+		default    : ;
 	endcase
 end
 
